731c3a385562bd71c8540bf9ee282db52bbb6632
Marcin Fortuna <marcin.fort@gmail.com>
2018-01-18 21:37:47 +0100

    Add another attempt at HackerRank 'Project Euler 12: Highly Divisible Triangular Number' challenge

--- hackerrank_pr_euler_12_highly_divisible_triangular_number.js
+++ hackerrank_pr_euler_12_highly_divisible_triangular_number.js
@@ -47,3 +47,60 @@ process.stdin.on("end", function () {
    processData(_input);
 });
 
+
+// Another attempt - this one builds an array with triangular numbers and another with their divisors so as to avoid looping through already computed numbers. However, the algorithm is still terminated due to timeout.
+
+
+function processData(input) {
+    //Enter your code here
+    var input_arr = input.split("\n").splice(1).map(x => Number(x));
+    var output_arr = [];
+    
+    function countDivisors(n) {
+        var divisorCounter = 1;
+        for (i = 1; i <= n/2; i++) {
+            if (n % i === 0) {
+                divisorCounter++;
+            }
+        }
+        return divisorCounter;
+    }
+    
+    var triangle_numbers = [1];
+    var number_divisors = [1];
+    
+    var currentTriangleNumber = 1;
+    var add = 1;
+    
+    input_arr.forEach(function(number) {
+        
+        if (number < number_divisors[number_divisors.length-1]) {
+           var index = number_divisors.findIndex(x => x > number);
+           output_arr.push(triangle_numbers[index]);
+        } else {
+           while (countDivisors(currentTriangleNumber) <= number) {
+            add += 1;
+            currentTriangleNumber += add;
+            triangle_numbers.push(currentTriangleNumber);
+            number_divisors.push(countDivisors(currentTriangleNumber));
+          }
+          output_arr.push(currentTriangleNumber);
+        }
+    });
+        
+    
+    console.log(output_arr.join("\n"));
+    
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
+
