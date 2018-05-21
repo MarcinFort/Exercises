bf7d8a77b5f53772d2f876cf6a0f4661dbe6167b
Marcin Fortuna <marcin.fort@gmail.com>
2018-05-21 23:03:56 +0200

    Assignment 4 from the course 'Programming for the Web with JavaScript', Step 2: Add initial font-weight and font-size rendering

--- edX - Programming for the web with JavaScript/FontChooser.js
+++ edX - Programming for the web with JavaScript/FontChooser.js
@@ -1,21 +1,26 @@
 class FontChooser extends React.Component {
 
     constructor(props) {
-	super(props);
+		super(props);
     }
     
 
     render() {
+		
+		const style = {
+			fontWeight: this.props.bold === 'false' ? 'normal' : 'bold',
+			fontSize: this.props.size
+		}
 
-	return(
+		return(
 	       <div>
 	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
 	       <button id="decreaseButton" hidden='true'>-</button>
 	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
 	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan">{this.props.text}</span>
+	       <span id="textSpan" style={style}>{this.props.text}</span>
 	       </div>
-	);
+		);
     }
 }
 
