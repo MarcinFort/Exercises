15b210e349009ec9addc63cda87fadd64e27ef50
Marcin Fortuna <marcin.fort@gmail.com>
2018-05-31 23:41:51 +0200

    Add a header

--- edX - Programming for the web with JavaScript/react-nytimes-app/src/App.js
+++ edX - Programming for the web with JavaScript/react-nytimes-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { ArticlesGrid } from './ArticlesGrid'
+import { Header } from './Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <ArticlesGrid />
       </div>
     );
