8da706c65e9c01619f957fae4037190b0df54cf8
Marcin Fortuna <marcin.fort@gmail.com>
2018-06-01 19:53:28 +0200

    Add titles and categories display to images

--- edX - Programming for the web with JavaScript/react-nytimes-app/src/ArticleWithImg.js
+++ edX - Programming for the web with JavaScript/react-nytimes-app/src/ArticleWithImg.js
@@ -4,13 +4,17 @@ export const ArticleWithImg = (props) => {
     let imgURL = 'https://static01.nyt.com/' + props.article.imageURL;
     return (
         <div className='article'>
-            <a className='article-link' href={props.article.webURL}>
-                <img className='article-image'
-                    alt={props.article.title}
-                    title={props.article.title}
-                    src={imgURL} 
-                />
-            </a>
+            <div class="article-img-container">
+                <a className='article-link' href={props.article.webURL}>
+                    <img className='article-image'
+                        alt={props.article.title}
+                        title={props.article.title}
+                        src={imgURL} 
+                    />
+                </a>
+            </div>
+            <div className="article-category">{props.article.type + " - " + props.article.section}</div>
+            <div className="article-caption">{props.article.title}</div>
         </div>
     );
 }
