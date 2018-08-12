f28df2f7101b8ff66baca392161f44741a991068
Marcin Fortuna <marcin.fort@gmail.com>
2018-08-12 02:14:06 +0200

    Add Ex10: Custom Video Player from JavaScript 30: Step 2 (Change the Icon; add the Skip function); refactoring

--- JavaScript30/Ex11 Custom Video Player/scripts.js
+++ JavaScript30/Ex11 Custom Video Player/scripts.js
@@ -3,13 +3,24 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 const togglePlay = () => {
     video.paused ? video.play() : video.pause();
 }
 
-[toggle, video].forEach(x => {
-    x.addEventListener('click', togglePlay);
-});
+const changeIcon = (e) => {
+    e.type === "play" ? toggle.innerHTML = "||" : toggle.innerHTML = "►";
+}
+
+const skip = (e) => {
+    let seconds = Number(e.target.dataset.skip);
+    video.currentTime += seconds;
+}
+
+[toggle, video].forEach(x => x.addEventListener('click', togglePlay));
+
+video.addEventListener("play", changeIcon);
+video.addEventListener("pause", changeIcon);
+skipButtons.forEach(x => x.addEventListener("click", skip));
