7b8c5c673a6a4a976d30ceca3a2e8a88e3ec758b
Marcin Fortuna <marcin.fort@gmail.com>
2018-08-12 12:06:37 +0200

    Add Ex10: Custom Video Player from JavaScript 30: Step 3: Add changing volume, pace and progress

--- JavaScript30/Ex11 Custom Video Player/scripts.js
+++ JavaScript30/Ex11 Custom Video Player/scripts.js
@@ -4,7 +4,9 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
+
+let mousedown = false;
 
 const togglePlay = () => {
     video.paused ? video.play() : video.pause();
@@ -19,8 +21,30 @@ const skip = (e) => {
     video.currentTime += seconds;
 }
 
+const handleRangeUpdate = (e) => {
+    video[e.target.name] = e.target.value;
+}
+
+const handleProgress = () => {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+const scrub = (e) => {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 [toggle, video].forEach(x => x.addEventListener('click', togglePlay));
 
 video.addEventListener("play", changeIcon);
 video.addEventListener("pause", changeIcon);
 skipButtons.forEach(x => x.addEventListener("click", skip));
+ranges.forEach(x => x.addEventListener('change', handleRangeUpdate));
+video.addEventListener("timeupdate", handleProgress);
+progress.addEventListener("click", scrub);
+
+document.addEventListener("mousedown", () => mousedown = true);
+document.addEventListener("mouseup", () => mousedown = false);
+
+progress.addEventListener("mousemove", (e) => {mousedown && scrub()});
